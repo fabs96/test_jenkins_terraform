3b0a304b0d5c75985d281986ce87a1a985882ecb
fabrizzio <fabrizzio1996@hotmail.es>
2021-07-16 12:55:44 -0500

    minor fix

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws-instance" "test-instance" {
+resource "aws_instance" "test-instance" {
   ami= "ami-02de934ca4f3289e0"
   instance_type = "t2.micro"
   tags = {
