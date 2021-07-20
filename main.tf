e3106e0a26cde2e48769528f0c2ef1158b69f8b4
fabrizzio <fabrizzio1996@hotmail.es>
2021-07-20 15:12:10 -0500

    fixed ubuntu

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "test-instance" {
-  ami= "data.aws_ami.ubuntu.id"
+  ami= data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
   tags = {
       Name = "test-instance"
